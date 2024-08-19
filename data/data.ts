b03d4019df0d9267eb4631a5e9ba429d9de5a5ae
cj-pexx <wenjie@pexx.com>
2024-08-19 14:44:44 +0800

    init

---
+++ data/data.ts
@@ -0,0 +1,31 @@
+/*
+Coding Task: Fetch Current City Weather Information
+Problem Statement:
+You are given three APIs:
+
+1. Get current IP:
+"https://api.ipify.org" or "https://ipinfo.io/ip"
+
+2. IP to City Code API: This API allows you to retrieve the city code based on the current IP 
+https://restapi.amap.com/v3/ip?ip=${ipcode}&key=2c017fc4c0b5629c24d5167a291cd1d3
+
+3. City Code to Weather API: This API provides the current day's weather information based on the city code.
+https://restapi.amap.com/v3/weather/weatherInfo?city=${citycode}&key=2c017fc4c0b5629c24d5167a291cd1d3
+
+
+Task:
+Write a function that outputs the current weather information of the current city.
+
+*/
+
+// https://restapi.amap.com/v3/ip?ip=${ipcode}&key=2c017fc4c0b5629c24d5167a291cd1d3
+// https://restapi.amap.com/v3/weather/weatherInfo?city=${citycode}&key=2c017fc4c0b5629c24d5167a291cd1d3
+
+const providers = ["https://api.ipify.org", "https://ipinfo.io/ip"]
+
+
+async function main() {
+    console.log("Interview");    
+}
+
+main();
